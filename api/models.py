2f7bbbed60abb5aeb89eabb41676c1b15aec47fc
Алексей <stormwww@yandex.ru>
2022-12-02 12:14:34 +0300

    model advert

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Advert(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Наименование')
+    description = models.TextField(blank=True, verbose_name='Описание товара')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    photo = models.JSONField(null=True)
